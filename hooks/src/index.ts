f6a93cc57296f88df15cdd444795936affeb7c78
shubhamxshah <ritikgujarat@gmail.com>
2025-11-26 01:17:56 +0530

    init

---
+++ hooks/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import {prisma} from "./lib/prisma";    
+
+const app = express();
+app.use(express.json());
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+
+    const body = req.body;
+
+    await prisma.$transaction(async (tx) => {
+        // Your transaction logic here
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            },
+        });
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+})

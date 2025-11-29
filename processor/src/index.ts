2d70159e513dd6c2c0a4c0ec253ba6f71d660193
shubhamxshah <ritikgujarat@gmail.com>
2025-11-30 00:44:42 +0530

    update processor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -13,19 +13,32 @@ async function main() {
     const producer = kafka.producer();
     await producer.connect();
 
-    while (1) {
-        const pendingRows = await prisma.zapRunOutbox.findMany({
-            where: {},
-            take: 10,
-        });
+    while (true) {
+        try {
+            const pendingRows = await prisma.zapRunOutbox.findMany({
+                where: {},
+                take: 10,
+            }); // fetches 10 at a time, if has lesser than 10, will fetch however many there are
 
+            if (pendingRows.length > 0) {
+                await producer.send({
+                    topic: TOPIC_NAME,
+                    messages: pendingRows.map(r => ({
+                        value: r.zapRunId,
+                    }))
+                });
 
-        producer.send({
-            topic: TOPIC_NAME,
-            messages: pendingRows.map(r => ({
-                value: r.zapRunId,
-            }))
-        })
+                // delete processed records
+                await prisma.zapRunOutbox.deleteMany({
+                    where: { id: { in: pendingRows.map(r => r.id) } }
+                });
+            } else {
+                // if no pending rows, wait for a while before checking again
+                await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second
+            }
+        } catch (error) {
+            console.error("Error processing zap runs:", error);
+        }
     }
 }
 

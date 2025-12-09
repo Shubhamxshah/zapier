90f3503829c6fb0bfe17f42eac3637e3fd964ec2
shubhamxshah <ritikgujarat@gmail.com>
2025-12-09 12:22:08 +0530

    prisma

---
+++ main-backend/prisma/migrations/20251209061104_image/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `metadata` on the `ZapRun` table. All the data in the column will be lost.
+  - Made the column `metadata` on table `Action` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `metadata` on table `Trigger` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Action" ALTER COLUMN "metadata" SET NOT NULL,
+ALTER COLUMN "metadata" SET DEFAULT '{}';
+
+-- AlterTable
+ALTER TABLE "Trigger" ALTER COLUMN "metadata" SET NOT NULL,
+ALTER COLUMN "metadata" SET DEFAULT '{}';
+
+-- AlterTable
+ALTER TABLE "ZapRun" DROP COLUMN "metadata";
